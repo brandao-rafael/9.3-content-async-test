c7fcbaaa87fb9cdb76181c38d5bda76c63445fd5
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-07-08 18:19:19 -0300

    atualizando

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -3,6 +3,7 @@ const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Teste a função fetchCharacter', () => {
   it('Verifica se o nome da personagem é Wonder Woman', async () => {
+//     expect.assertions(1)
     const character = await fetchCharacter('720');
     expect(character.name).toBe('Wonder Woman');
   });
